1de91ad4b835f16861fbe719976dd03244ffc008
Shaan Patel <shaanpatel00@gmail.com>
2019-08-02 20:22:44 -0400

    Initial commit

---
+++ src/main/kotlin/demo/Model.kt
@@ -0,0 +1,8 @@
+package demo
+
+import javax.persistence.*
+
+@Entity
+data class Movie(@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+                 val Id: Long,
+                 val name: String)
